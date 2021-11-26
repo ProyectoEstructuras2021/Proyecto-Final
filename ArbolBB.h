bf171489f20bf9a62b3f3f944d22ccf3c0a78516
Fausto-Mendez <85911046+Fausto-Mendez@users.noreply.github.com>
2021-11-26 16:10:36 -0600

    Update ArbolBB.h

--- ArbolBB.h
+++ ArbolBB.h
@@ -2,10 +2,15 @@
 #include "Nodo.h"
 #include <string>
 #include <sstream>
+#include <windows.h>
 
 using namespace std;
 
 class ArbolBB{
+	
+	int ar;
+	string str;//agregue sto
+	stringstream ss;//y esto
 	
 	private:
 		
@@ -36,8 +41,12 @@ class ArbolBB{
 		void insertar(int n){
 			Nodo *nuevo, *aux;
 			nuevo = nuevo->Crear(n);
+			ar=nuevo->dato;
+			ss >> ar;  //de aqui
+  			ss << str;
+			cout<<"ar "<<str;//a aqui
 		
-			if(arbolVacio())	//árbol es vacío
+			if(arbolVacio())	//Ã¡rbol es vacÃ­o
 				raiz = nuevo;
 			else {
 				siguienteNodo(raiz, nuevo);
@@ -64,7 +73,7 @@ class ArbolBB{
 		}
 		
 		void mostrarArbol(Nodo *aux, int cont){
-			if(aux==NULL){	//Si el arbol está vacío
+			if(aux==NULL){	//Si el arbol estÃ¡ vacÃ­o
 				return;
 			}
 			else{
@@ -76,4 +85,76 @@ class ArbolBB{
 				mostrarArbol(aux->izq, cont+1);
 			}
 		}
+		
+		void getArbol(Nodo* aux, int cont) {  //Ãrbol acostado
+  			if (aux == NULL){
+   			   return;
+   			}
+  			else {
+   				getArbol(raiz->der, cont + 1);
+   				cout << aux->getDato() << endl;
+   				for (int i = 0; i < cont; i++) {
+
+   				}
+  			}
+ 		}
+
+
+ 		void gotoxy(int x, int y) {
+ 			 HANDLE hcon;
+   			 hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+  			 COORD dwPos;
+			 dwPos.X = x;
+   			 dwPos.Y = y;
+ 			 SetConsoleCursorPosition(hcon, dwPos);
+ 		}
+
+ 		void background() {
+  			int i = 2;
+  			for (int y = 0; y < 7; y++) {
+  				 gotoxy(0, i);
+  				 cout << " Nivel " << y;
+   			for (int x = 8; x < 135; x++) {
+    			gotoxy(x, i);
+    			cout << "- ";
+   			}		
+   				i = i + 3;
+  			}
+ 		}
+
+ void Show(Nodo* aux, int x, int y, int level) {
+  if (aux == NULL)
+   	return;
+  else {
+   gotoxy(x, y);
+   cout << aux->getDato();
+   switch (level) {
+    case 0:
+     Show(aux->getIzq(), x - 30, y + 3, level + 1);
+     Show(aux->getDer(), x + 30, y + 3, level + 1);
+     break;
+    case 1:
+     Show(aux->getIzq(), x - 12, y + 3, level + 1);
+     Show(aux->getDer(), x + 12, y + 3, level + 1);
+     break;
+    case 2:
+     Show(aux->getIzq(), x - 7, y + 3, level + 1);
+     Show(aux->getDer(), x + 7, y + 3, level + 1);
+     break;
+    case 3:
+     Show(aux->getIzq(), x - 4, y + 3, level + 1);
+     Show(aux->getDer(), x + 4, y + 3, level + 1);
+     break;
+    case 4:
+     Show(aux->getIzq(), x - 2, y + 3, level + 1);
+     Show(aux->getDer(), x + 2, y + 3, level + 1);
+     break;
+    case 5:
+     Show(aux->getIzq(), x - 2, y + 3, level + 1);
+     Show(aux->getDer(), x + 2, y + 3, level + 1);
+     break;
+   }
+  }
+ }
+	
 };
